b60b2341dc16bf3028f1536a1022ec9de8bd307b
Lê Hùng Thiện <hungthien9394@gmail.com>
2014-03-20 19:11:04 +0700

    Finish class PersonList implementation

--- src/cj_workshop3/Person/PersonList.java
+++ src/cj_workshop3/Person/PersonList.java
@@ -15,5 +15,19 @@ package cj_workshop3.Person;
  * @author thienlh
  */
 public class PersonList {
-
+    //  Array of people
+    private Person[] list = null;
+    private int count=0;    //  Current index
+    //  Constructor
+    public PersonList(int size) {
+        if (size<10) size=10;
+        list = new Person[size];
+    }
+    //  Method for find person using their code
+    public int find(String qCode)   {
+        for (int i = 0; i < count; i++) {
+            if (qCode.equals(list[i].getCode())) return i;
+        }
+        return -1;
+    }
 }
