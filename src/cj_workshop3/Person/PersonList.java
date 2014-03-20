eb873a7d3e1c499edee1434580419ab1f4ae20e9
Lê Hùng Thiện <hungthien9394@gmail.com>
2014-03-20 19:39:23 +0700

    Update class PersonList
    Add new methods: find(), isWritable(), isEmpty(), remove(), add().

--- src/cj_workshop3/Person/PersonList.java
+++ src/cj_workshop3/Person/PersonList.java
@@ -7,8 +7,9 @@
  * Project  : Workshop 3 (cj_workshop3)
  * Date     : Mar 20 2014
  */
-
 package cj_workshop3.Person;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,18 +17,93 @@ package cj_workshop3.Person;
  */
 public class PersonList {
     //  Array of people
+
     private Person[] list = null;
-    private int count=0;    //  Current index
+    private int count = 0;    //  Current index
     //  Constructor
+
     public PersonList(int size) {
-        if (size<10) size=10;
+        if (size < 10) {
+            size = 10;
+        }
         list = new Person[size];
     }
     //  Method for find person using their code
-    public int find(String qCode)   {
+
+    public int find(String qCode) {
         for (int i = 0; i < count; i++) {
-            if (qCode.equals(list[i].getCode())) return i;
+            if (qCode.equals(list[i].getCode())) {
+                return i;
+            }
         }
         return -1;
+    }
+    //  Method for checking if the list is writeble (full) or not
+
+    public boolean isWritable() {
+        boolean result = true;
+        if (count == list.length) {
+            result = false;
+        }
+        return result;
+    }
+    //  Method for checking whether the list is empty or not
+
+    public boolean isEmpty() {
+        boolean result = false;
+        if (count == 0) {
+            result = true;
+        }
+        return result;
+    }
+    //  Method for adding person to list
+
+    public void add() {
+        if (isWritable()) {
+            String newCode;
+            String newName;
+            String newAddr;
+            float newMark;
+            //  Adding new person detail
+            Scanner sc = new Scanner(System.in);
+            int pos = 0;    //  Variable for checking whether newCode is existed or not.
+            do {
+                System.out.print("Enter the person's code: ");
+                newCode = sc.nextLine().toUpperCase();
+                pos = find(newCode);
+                if (pos >= 0) {
+                    System.out.println("This person is existed!");
+                }
+            } while (pos >= 0);
+            System.out.print("Enter the person's name: ");
+            newName = sc.nextLine().toUpperCase();
+            System.out.print("Enter the person's address: ");
+            newAddr = sc.nextLine().toUpperCase();
+            System.out.print("Enter the person's mark: ");
+            newMark = sc.nextFloat();
+            list[count++] = new Person(newCode, newName, newAddr, newMark);
+            System.out.println("New person has been added!");
+        } else System.out.println("The list is full!");
+    }
+    //  Method for remove a known-code person
+
+    public void remove() {
+        if (isEmpty()) {
+            System.out.println("The list is empty");
+            return;
+        }
+        String removingCode;    //  Code for removing
+        //  Enter a person's detail
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the code of removing person: ");
+        removingCode = sc.nextLine().toUpperCase();
+        int pos = find(removingCode);
+        if (pos<0)  System.out.println("This person does not exist!");
+        else    {
+            //  Shift up the remainder of the list
+            for (int i = 0; i < count-1; i++) list[i] = list[i+1];
+            count--;
+            System.out.println("The person " + list[pos].getName() + "(" + removingCode + ") was removed!");
+        }
     }
 }
